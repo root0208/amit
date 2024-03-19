7a7873040de8ed031d2d0d55e564435685ae0096
Amit Gupta <amitgupta.cse19@gmail.com>
2024-03-19 12:10:41 +0530

    modified code for use state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 function App() {
   return (
     <>
       <Navbar title= "My App" aboutText="About us"/>
       {/* <Navbar/> */}
+      <div className="container my-3">
+      <TextForm heading="Enter the text to analyze below"/>
+      </div>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,12 +1,13 @@
-import React from 'react'
-
-export default function TextForm() {
+import React,{useState} from 'react'
+export default function TextForm(props) {
+  const [text, setText] = useState('Enter text here');
   return (
     <div>
+      <h1>{props.heading}</h1>
         <div className="mb-3">
-        <label for="myBox" className="form-label">Example textarea</label>
-        <textarea className="form-control" id="myBox" rows="3"></textarea>
+        <textarea className="form-control" value={text} id="myBox" rows="8"></textarea>
         </div>
+        <button className="btn btn-primary">Convert to uppercase</button>
     </div>
   )
 }
